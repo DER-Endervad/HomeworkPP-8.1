670d61c243c7cf29918152430c191477e69535ce
Даниил <derendervad@gmail.com>
2023-03-12 13:00:19 +0300

    initial commit

---
+++ HomeworkPP 8.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+template<class T>
+void output(T t) {
+    for (const auto& elem : t) {
+        std::cout << " " << elem;
+    }
+}
+template<class T>
+void removing_duplicates(T &vec) {
+    int x = 0;
+
+    for (int i = 0; i < vec.size(); ++i) {
+        x = vec[i];
+        auto iter = vec.cbegin();
+        for (int j = i + 1; j < vec.size(); ++j) {
+            if (vec[j] == x) {
+                vec.erase(iter+j);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]:";
+    output(vec); std::cout << "\n" << std::endl;
+    
+    removing_duplicates(vec);
+    std::cout << "[OUT]:";
+    output(vec); std::cout << "\n" << std::endl;
+}
